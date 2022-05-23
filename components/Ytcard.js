30f7defe463e11a4ab3e7e1ad005ddcc87802e92
tarunclub <tarunkm__@outlook.com>
2022-05-23 17:27:03 +0530

    homepage v2

--- components/Ytcard.js
+++ components/Ytcard.js
@@ -7,9 +7,9 @@ function MediumCard({ link, title }) {
           height="250"
           src={link}
           title="YouTube video player"
-          frameborder="0"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
+          allowFullScreen
           className="rounded-lg"
         ></iframe>
       </div>
